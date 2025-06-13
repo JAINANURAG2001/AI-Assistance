59a9bae569e0c8c9864e7467415e0c187c82bf5f
Anurag Jain <jainanurag7nov@gmail.com>
2025-06-13 09:57:45 +0530

    feat: Readme and other features added

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,22 @@
+import { groq } from '@ai-sdk/groq';
+import {streamText } from 'ai';
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.GROQ_API_KEY,
+  baseURL: "https://api.groq.com/openai/v1"
+});
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: groq('llama-3.3-70b-versatile'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
