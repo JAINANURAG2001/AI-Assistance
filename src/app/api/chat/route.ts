d876219ef19f172aab1196537360d23468e64e07
Anurag Jain <jainanurag7nov@gmail.com>
2025-06-13 10:08:30 +0530

    client function removed

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,11 +1,5 @@
 import { groq } from '@ai-sdk/groq';
 import {streamText } from 'ai';
-import OpenAI from "openai";
-
-const client = new OpenAI({
-  apiKey: process.env.GROQ_API_KEY,
-  baseURL: "https://api.groq.com/openai/v1"
-});
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
